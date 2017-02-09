8d35ac4f6b2caac7f68218461418d035b9dfe82a
rriosalido <rriosalido@icloud.com>
2017-02-09 13:16:21 +0100

    Version 0.1 incluido export to CSV y sendMail

--- TableViewController.swift
+++ TableViewController.swift
@@ -100,6 +100,7 @@ class TableViewController: UITableViewController, WCSessionDelegate, MFMailCompo
     
     
     
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
